81a002cbf3bdc22d12c6067eb7a0bdfa1e1622a2
tung nguyen <nguyenthanhtunghanuk14@gmail.com>
2018-03-12 22:12:48 +0700

    create database

---
+++ database/migrations/2018_03_12_132310_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('service_tag');
+            $table->string('name');
+            $table->text('detail');
+            $table->string('img_url');
+            $table->timestamps('create_at');
+        });
+
+        Schema::table('products', function ($table) {
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
